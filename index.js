fff391529ae9c2df68eaca74ea4d22db28559598
John Pieri <jvpieri@gmail.com>
2023-04-11 17:25:15 -0400

    Clarified code. Added Comments

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const massDisplay = document.querySelector("#mass-El p")
 const checkbox = document.getElementById("switch-style")
 const stylesheet1 = document.getElementById("stylesheet1")
 
+// Units
+
 const unitConversions = {
     "meters to feet": {
         convert: function(meters) {
@@ -39,12 +41,16 @@ const unitConversions = {
    }
 };
 
+// Convert function
+
 function performConversions() {
     const value = valueInput.value
     lengthDisplay.textContent = `${value} meters = ${unitConversions["meters to feet"].convert(value).toFixed(3)} feet | ${value} feet = ${unitConversions["feet to meters"].convert(value).toFixed(3)} meters`
     volumeDisplay.textContent = `${value} liters = ${unitConversions["liters to gallons"].convert(value).toFixed(3)} gallons | ${value} gallons = ${unitConversions["gallons to liters"].convert(value).toFixed(3)} liters`
     massDisplay.textContent =  `${value} kilos = ${unitConversions["kilograms to pounds"].convert(value).toFixed(3)} pounds | ${value} pounds = ${unitConversions["pounds to kilograms"].convert(value).toFixed(3)} kilos`
 }
+
+// Click convert when enter key is pressed
 
 valueInput.addEventListener("keyup", function(event) {
     if (event.key === 'Enter') {
